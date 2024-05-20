f1d689956044314a6ac3f0cd05b482c63df7cb42
He Yiyang <161405006+1289850360@users.noreply.github.com>
2024-05-20 21:04:40 +0800

    Add files via upload

---
+++ Practical 9/IBI1_student_database.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(students, name, major, code_portfolio_score, group_project_score, exam_score):
+        students.name = name
+        students.major = major
+        students.code_portfolio_score = code_portfolio_score
+        students.group_project_score = group_project_score
+        students.exam_score = exam_score
+
+    def print_details(students):
+        print('Name: {}, Major: {}, Code Portfolio Score: {}, Group Project Score: {}, Exam Score: {}'.format(
+            students.name, students.major, students.code_portfolio_score, students.group_project_score, students.exam_score))
+
+# Example
+student1 = Student('He Yiyang', 'BMI', 100, 100, 100)
+student1.print_details()

2f417361fba0628b246f3f1fb2ea96aa76db0adf
He Yiyang <yiyang.23@intl.zju.edu.cn>
2024-05-21 11:05:57 +0800

    Update get_duplicate_genes.py

--- Practical 8/get_duplicate_genes.py
+++ Practical 8/get_duplicate_genes.py
@@ -1,26 +1,18 @@
-import re  
-  
-def process_fasta(input_file, output_file):  
-    gene_name = None  
-    current_seq = []  
-    with open(input_file, 'r', encoding="UTF-8") as f:  
-        for line in f:  
-            if line.startswith('>'):  
-                if gene_name and current_seq:  
-                    if 'duplication' in gene_name:  
-                        yield gene_name, ''.join(current_seq).strip()  
-                    current_seq.clear()   
-                gene_name = line.strip('>').strip()  
-            else:  
-                current_seq.append(line.strip())  
-        if gene_name and current_seq:  
-            if 'duplication' in gene_name:  
-                yield gene_name, ''.join(current_seq).strip()  
-  
-with open("duplicate_genes.fa", 'w', encoding="UTF-8") as output_file:  
-    for gene_name, seq in process_fasta("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa", ""):  
-        if 'duplication' in gene_name:  
-            output_file.write(f">{gene_name}_mRNA\n{seq}\n")  
- 
-with open('duplicate_genes.fa', 'r', encoding="UTF-8") as output_file:  
+
+import re
+
+with open("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa", 'r', encoding="UTF-8") as f:
+    a=f.read()
+    names=re.findall(r"gene:(\S+).*duplication",a)
+    seqs = re.findall(r'duplication.+\n((?:[^>].*\n)+)', a)
+    dict = {name: seq for name, seq in zip(names, seqs)}
+    output_str = ""
+    
+    for key, value in dict.items():
+        output_str += f">{key}_mRNA\n{value}\n"
+
+with open('duplicate_genes.fa', 'w', encoding="UTF-8") as output_file:
+    output_file.write(output_str)
+
+with open('duplicate_genes.fa', 'r', encoding="UTF-8") as output_file:
     print(output_file.read())
